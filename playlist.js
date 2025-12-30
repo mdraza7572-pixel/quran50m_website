d78b48b89ca7673bf448b8d395ea4929ac616347
mdraza7572-pixel <mdraza7572@gmail.com>
2025-12-30 19:39:57 +0530

    Create playlist.js with video details
    Added a list of video links with titles and descriptions.

---
+++ playlist.js
@@ -0,0 +1,25 @@
+
+// Yahan apni videos ki list update karo
+const myVideos = [
+    {
+        link: "https://youtu.be/rTw1qqAngzk?si=_9ksduUZ04BGfsr1",
+        title: "Surah Yaseen",
+        desc: "Dil ka sukoon aur maghfirat ke liye."
+    },
+    {
+        link: "https://youtu.be/3g6vfftQ-z0?si=dmxHQ86huNb7U4sT",
+        title: "Durood Kushadgi e Rizq",
+        desc: "Rizq mein barkat aur kushadgi."
+    },
+    {
+        link: "https://youtu.be/ryE4r6RQD6Q?si=aWDqmTcrAgvKhJzh",
+        title: "Surah Baqarah",
+        desc: "Karobar aur tijarat mein barkat."
+    },
+    {
+        link: "https://youtu.be/68yBHe0L1T4?si=VV7ptR_YpAo4wpMQ",
+        title: "Allah Ke 99 Naam",
+        desc: "Asma-ul-Husna ki fazilat."
+    },
+    // Nayi video add karne ke liye bas upar wala hissa copy-paste karo
+];
