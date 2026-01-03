6202362360d21a2aa623f2776b8370d0f86f68fb
mdraza7572-pixel <mdraza7572@gmail.com>
2026-01-03 09:38:55 +0530

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  alert("DOM READY");
 
-  const sendBtn = document.getElementById("sendBtn");
-  const micBtn = document.getElementById("micBtn");
-
-  if (sendBtn) sendBtn.onclick = () => alert("Send working");
-  if (micBtn) micBtn.onclick = () => alert("Mic working");
-});
